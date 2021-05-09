561253cecb46d91726ff09b7bd643dda7065dd7b
Hritik Raj <hritik99@gmail.com>
2021-05-09 17:16:14 -0500

    Initial Commit

---
+++ MusicVisualizer/MusicVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicVisualizerApp.swift
+//  MusicVisualizer
+//
+//  Created by Hritik Raj on 5/9/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
